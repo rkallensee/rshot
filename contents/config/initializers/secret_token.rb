f611d5b0624671eff04c2a4a958ba97b9350fa77
Raphael Kallensee <raphael@kallensee.name>
2010-11-13 22:27:27 +0100

    Added empty Rails 3 project.

---
+++ contents/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Openshot::Application.config.secret_token = 'cf61f14b7eaf29ef979e864f9a9114b05a44b5d6489ddd1d473684614c41947ea07fd0546048ea3875807d7d01f9f008f21320238e8bf3d73a3af17342dcd459'

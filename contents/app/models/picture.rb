a97c81fab82869c2b794bc466d55ba668d75a0f2
Raphael Kallensee <raphael@kallensee.name>
2010-11-14 17:54:16 +0100

    Further improvements, new attachment style for detail view.

--- contents/app/models/picture.rb
+++ contents/app/models/picture.rb
@@ -5,12 +5,14 @@ class Picture < ActiveRecord::Base
   validates :title, :presence => true,
                     :length => { :minimum => 3 }
   validates_attachment_presence :photo
+  validates_attachment_size :photo, :less_than => 5.megabytes  
 
   # Paperclip attachment
   has_attached_file :photo,
     :styles => {
       :thumb=> ["75x75#", :jpg],
-      :small  => ["230x230>", :jpg] }
+      :small  => ["230x230>", :jpg],
+      :medium  => ["640x640>", :jpg] }
       
   def album_name
     unless album.nil?

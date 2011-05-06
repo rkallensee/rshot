ca74e378c7536c28c951c8e73137bff74aee5fbd
Raphael Kallensee <raphael@kallensee.name>
2011-05-07 01:46:42 +0200

    Added logo. Added rough implementation of map display when there are geo-tags inside of the image.

--- contents/app/controllers/pictures_controller.rb
+++ contents/app/controllers/pictures_controller.rb
@@ -64,6 +64,7 @@ class PicturesController < ApplicationController
         format.html { redirect_to(@picture, :notice => 'Picture was successfully created.') }
         format.xml  { render :xml => @picture, :status => :created, :location => @picture }
       else
+        @albums = Album.where("user_id" => current_user.id).order("title ASC")
         format.html { render :action => "new" }
         format.xml  { render :xml => @picture.errors, :status => :unprocessable_entity }
       end
--- contents/app/models/picture.rb
+++ contents/app/models/picture.rb
@@ -5,14 +5,15 @@ class Picture < ActiveRecord::Base
   # validation
   validates :title, :presence => true, :length => { :minimum => 3 }
   validates_attachment_presence :photo
-  validates_attachment_size :photo, :less_than => 5.megabytes  
+  validates_attachment_size :photo, :less_than => 10.megabytes  
 
   # Paperclip attachment
   has_attached_file :photo,
     :styles => {
       :thumb   => ["75x75#", :jpg],
       :small   => ["230x230>", :jpg],
-      :medium  => ["640x640>", :jpg] }
+      :medium  => ["640x640>", :jpg] },
+    :convert_options => { :all => '-auto-orient' }
       
   # scopes
   scope :same_album, lambda { |att| {:conditions => ["album_id = ?", att]} }
@@ -28,5 +29,20 @@ class Picture < ActiveRecord::Base
   def self.random
     offset = rand(self.count)
     rand_record = self.first(:offset => offset)
+  end
+  
+  def gps
+    imgexif = EXIFR::JPEG.new(photo.path)
+    #imgexif.gps if imgexif.gps?
+    #[imgexif.gps_latitude, imgexif.gps_longitude]
+    
+    # https://github.com/picuous/exifr/blob/master/lib/jpeg.rb
+    if imgexif.gps_latitude.nil? or imgexif.gps_latitude.length < 2 or imgexif.gps_longitude.nil? or imgexif.gps_longitude.length < 2
+      nil
+    else
+      lat = imgexif.gps_latitude[0].to_f+imgexif.gps_latitude[1].to_f/60+imgexif.gps_latitude[2].to_f/3600
+      lon = imgexif.gps_longitude[0].to_f+imgexif.gps_longitude[1].to_f/60+imgexif.gps_longitude[2].to_f/3600
+      [lat, lon]
+    end
   end
 end

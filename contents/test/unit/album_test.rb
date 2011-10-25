221b60e24f8f0c73cb887f77d4d6d04035891bd4
Raphael Kallensee <raphael@kallensee.name>
2011-10-25 23:36:16 +0200

    It's a shame, but touched tests for the first time. Added fixtures for all current models and started with albums unit test. (#116)

--- contents/test/unit/album_test.rb
+++ contents/test/unit/album_test.rb
@@ -2,7 +2,14 @@ require 'test_helper'
 
 class AlbumTest < ActiveSupport::TestCase
   # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "validate album title length" do
+    album = Album.new
+    assert !album.save
+
+    album = Album.new({:title => "12"})
+    assert !album.save
+
+    album = Album.new({:title => "123"})
+    assert album.save
   end
 end

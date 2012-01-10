80f326681347606980b5c3415d25c05cf2c8901b
Raphael Kallensee <raphael@kallensee.name>
2012-01-11 00:01:17 +0100

    Slightly improved the Ability model for CanCan, smaller view enhancements.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -22,23 +22,38 @@ class Ability
   def initialize(user)
     user ||= User.new # guest user (not logged in)
 
-    # every user can manage its own pictures and albums
-    can :manage, [Picture, Album] do |pa, params|
-      pa.try(:profile).try(:user) == user
+    # important note from the CanCan wiki: "The block is only evaluated when an
+    # actual instance object is present. It is not evaluated when checking
+    # permissions on the class (such as in the index action). This means any
+    # conditions which are not dependent on the object attributes should be
+    # moved outside of the block."
+
+    # every user can manage its own albums
+    can :manage, Album do |album|
+      album.try(:profile).try(:user) == user
     end
 
-    # but a picture may only assigned to a users' own album
-    can :create, Picture do |picture, params|
-      picture.try(:profile).try(:user) == user &&
-      ( picture.album.nil? ||
-        picture.album.try(:profile).try(:user) == user )
+    # pictures may get created by registered users
+    can :new, Picture, user.id > 0
+
+    # pictures may only get edited or destroyed by their owners
+    can [:edit, :destroy], Picture do |picture|
+      picture.try(:profile).try(:user) == user
     end
-    can :update, Picture do |picture, params|
-      picture.try(:profile).try(:user) == user &&
-      ( ( picture.album.nil? && ( params.nil? || params[:album_id].nil? ) ) ||
-        ( picture.album.try(:profile).try(:user) == user &&
-          ( params.nil? || params[:album_id].nil? ||
-            ( Album.exists?(params[:album_id]) && Album.find(params[:album_id]).try(:profile).try(:user) == user ) ) ) )
+
+    # pictures may get updated or created by their owner,
+    # but may only get attached to users' own albums.
+    can [:create, :update], Picture do |picture, params|
+      unless picture.try(:profile).try(:user) == user
+        false # skip if picture doesn't belong to user'
+      else
+        if picture.album.nil? && ( params.nil? || params[:album_id].nil? )
+          true # no album - permitted
+        else
+          ( picture.album.nil? || picture.album.try(:profile).try(:user) == user ) &&
+          ( params.nil? || params[:album_id].nil? || ( Album.exists?(params[:album_id]) && Album.find(params[:album_id]).try(:profile).try(:user) == user ) )
+        end
+      end
     end
 
     # comment creation is allowed for all logged-in users

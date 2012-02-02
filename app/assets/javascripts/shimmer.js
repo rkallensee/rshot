85ffae6e733fdec4a2af4f95facc88bde5d30142
Raphael Kallensee <raphael@kallensee.name>
2012-02-02 13:46:39 +0100

    Upgraded to Bootstrap 2.0, fixed first issues and used new markup/elements.

--- app/assets/javascripts/shimmer.js
+++ app/assets/javascripts/shimmer.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
-    $('a[rel=twipsy], span[rel=twipsy]').twipsy({'placement': 'below'});
+    $('a[rel=tooltip], span[rel=tooltip]').tooltip();
+
     $('a[rel=popover]').popover({html: true});
 
     $("a.lightbox").fancybox({
